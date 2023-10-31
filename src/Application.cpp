aa28206c7abbf0e0fd8f5ba2254aa993b57cad83
1839658926@qq.com <1839658926@qq.com>
2023-10-31 16:24:11 +0800

    feat: 建立一个测试框架，对Application程序进行重构;

--- src/Application.cpp
+++ src/Application.cpp
@@ -20,6 +20,8 @@
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw_gl3.h"
 
+#include "tests/TestClearColor.h"
+
 
 int main(void)
 {
@@ -53,52 +55,8 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
-
-        float positions[] = {
-             100.5f,  100.5f, 0.0f, 0.0f, //0
-             200.5f,  100.5f, 1.0f, 0.0f, //1
-             200.5f,  200.5f, 1.0f, 1.0f, //2
-             100.5f,  200.5f, 0.0f, 1.0f  //3
-        };
-
-        unsigned int indices[] = {
-            0, 1, 2,
-            2, 3, 0
-        };
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
         GLCall(glEnable(GL_BLEND));
-
-        VertexArray va;
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-        VertBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBufer(vb, layout);
-
-
-        IndexBuffer ib(indices, 6);
-
-        //投影矩阵
-        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-
-        //视图矩阵
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-
-
-        Shader shader("res/shader/Basic.shader");
-        shader.Bind();
-        shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-        
-
-        Texture texture("res/textures/jinliu.png");
-        texture.Bind();
-        shader.SetUniform1i("u_Texture", 0);
-
-        va.UnBind();
-        shader.UnBind();
-        vb.UnBind();
-        ib.UnBind();
 
         Renderer renderer;
 
@@ -106,46 +64,24 @@ int main(void)
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
 
-        glm::vec3 translation(200, 200, 0);
+        test::TestClearColor test;
 
-        float r = 0.0f;
-        float increment = 0.05f;
-        /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             renderer.Clear();
 
+            test.OnUpdate(0.0f);
+            test.OnRender();
+
             ImGui_ImplGlfwGL3_NewFrame();
-
-            //模型矩阵
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-            glm::mat4 mvp = proj * view * model;
-
-            shader.Bind();
-            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-            shader.SetUniformMat4f("u_MVP", mvp);
-
-            renderer.Draw(va, ib, shader);
-
-            if (r > 1.0f)
-                increment = -0.05f;
-            else if (r < 0.0f)
-                increment = 0.05f;
-
-            r += increment;
-
-            {                          
-                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            }
+            
+            test.OnImGuiRender();
 
             ImGui::Render();
             ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
-             /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
-            /* Poll for and process events */
             glfwPollEvents();
         }
     }
